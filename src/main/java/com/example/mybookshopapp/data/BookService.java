3161e877711135e7a5c1525d4b9a219d92cb02e5
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-03-13 20:12:40 +0300

    Таблица author 🦾🦾🦾
    Сейчас на странице “Авторы” отображаются статические данные, прописанные прямо в html-коде страницы. Нам необходимо заменить эти статические значения данными из нашей базы. Преобразуйте базу данных приложения, создав таблицу author с данными авторов  и свяжите ее с уже имеющейся таблицей books. Добавьте необходимую логику для вывода данных списка рекомендуемых книг на странице “Главная”, а также, данные списка авторов на странице “Авторы”.

--- src/main/java/com/example/mybookshopapp/data/BookService.java
+++ src/main/java/com/example/mybookshopapp/data/BookService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -19,15 +21,30 @@ public class BookService {
     }
 
     public List<Book> getBookData() {
-        List<Book> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rowNum) -> {
+        List<Book> books = jdbcTemplate.query("SELECT authors.id, title, priceOld, price, authorName FROM books LEFT JOIN authors on books.authorId = authors.id", (ResultSet rs, int rowNum) -> {
             Book book = new Book();
             book.setId(rs.getInt("id"));
-            book.setAuthor(rs.getString("author"));
+            book.setAuthor(rs.getString("authorName"));
             book.setTitle(rs.getString("title"));
             book.setPriceOld(rs.getString("priceOld"));
             book.setPrice(rs.getString("price"));
             return book;
         });
         return new ArrayList<>(books);
+    }
+
+    public Map<Character, List<Author>> getAuthorData() {
+        List<Author> authors = jdbcTemplate.query("SELECT id, authorName from authors ORDER BY authorName", (ResultSet rs, int rowNum) -> {
+            Author author = new Author();
+            author.setId(rs.getInt("id"));
+            author.setAuthorName(rs.getString("authorName"));
+            return author;
+        });
+        return authors.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                author -> author.getAuthorName().charAt(0)
+                        )
+                );
     }
 }
